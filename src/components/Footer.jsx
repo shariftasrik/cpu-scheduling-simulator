0c91c7772d09515990714f065296731c459f80c5
Nimur Rahman Sharif Tasrik <126714137+shariftasrik@users.noreply.github.com>
2025-07-12 19:57:59 +0600

    linkedin and github accessible logo added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,31 @@
-export default function Footer(){
+import { Github, Linkedin } from "lucide-react";
+
+export default function Footer() {
   const year = new Date().getFullYear();
-    return (
+  return (
     <footer className="bg-white border-t border-gray-200 mt-12">
       <div className="max-w-7xl mx-auto px-6 py-4 flex flex-col md:flex-row items-center justify-between text-gray-600 text-sm">
-        <p className="mb-2 md:mb-0">
-          CPU Scheduling Simulator
-        </p>
+        <p className="mb-2 md:mb-0">CPU Scheduling Simulator</p>
         <p>Tasrik © {year} </p>
         <div className="flex space-x-4">
+          <a
+            href="linkedin.com/in/nimur-rahman-sharif-946108349"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-blue-600 transition-colors"
+          >
+            <Linkedin className="w-5 h-5" />
+          </a>
           <a
             href="https://github.com/shariftasrik/cpu-scheduling-simulator.git"
             target="_blank"
             rel="noopener noreferrer"
             className="hover:text-blue-600 transition-colors"
           >
-            GitHub
+            <Github className="w-5 h-5" />
           </a>
         </div>
       </div>
     </footer>
-    );
+  );
 }
